ac72188cc14b2e59e0add900ed35533a8995a9c3
Danijel Vincijanovic <danijel.vincijanovic@gmail.com>
2026-03-04 11:30:16 +0100

    Use studio URL from search params

--- src/components/providers/LivePreview.tsx
+++ src/components/providers/LivePreview.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import dynamic from "next/dynamic";
 
 const HygraphPreview = dynamic(
@@ -22,6 +22,7 @@ function checkIsInIframe(): boolean {
 
 export function LivePreview({ children }: { children: React.ReactNode }) {
   const router = useRouter();
+  const searchParams = useSearchParams();
   const [isInIframe, setIsInIframe] = useState(false);
 
   useEffect(() => {
@@ -30,11 +31,13 @@ export function LivePreview({ children }: { children: React.ReactNode }) {
 
   if (!isInIframe) return <>{children}</>;
 
+  const studioUrl = searchParams.get("origin") ?? undefined;
+
   return (
     <HygraphPreview
       endpoint={process.env.NEXT_PUBLIC_HYGRAPH_CONTENT_ENDPOINT as string}
       onSave={() => router.refresh()}
-      studioUrl="https://studio-eu-central-1-staging.hygraph.com"
+      studioUrl={studioUrl}
       debug={process.env.NODE_ENV === "development"}
       sync={{
         fieldFocus: true,
